b8f90a06dc395163c6bdb67d53cf6406a290fcb1
ZhaohengLi <zhaohengli.thu@gmail.com>
2019-08-06 22:41:28 +0800

    update

--- sentence_distance.py
+++ sentence_distance.py
@@ -3,7 +3,7 @@ import jieba
 import numpy as np
 from scipy.linalg import norm
 
-model_file = 'word2vec/news_12g_baidubaike_20g_novel_90g_embedding_64.bin'
+model_file = './data/word2vec/news_12g_baidubaike_20g_novel_90g_embedding_64.bin'
 model = gensim.models.KeyedVectors.load_word2vec_format(model_file, binary=True)
 
 
@@ -21,15 +21,3 @@ def vector_similarity(s1, s2):
     return np.dot(v1, v2) / (norm(v1) * norm(v2))
 
 
-strings = [
-    '你在干什么',
-    '你在干啥子',
-    '你在做什么',
-    '你好啊',
-    '我喜欢吃香蕉'
-]
-
-target = '你在干啥'
-
-for string in strings:
-    print(string, vector_similarity(string, target))
