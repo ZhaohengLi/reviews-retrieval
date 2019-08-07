36451bbd231a798a0bfe85ddeecc85c0ab7f2ee2
ZhaohengLi <zhaohengli.thu@gmail.com>
2019-08-07 11:02:13 +0800

    Reformat code.

--- sentence_distance.py
+++ sentence_distance.py
@@ -3,11 +3,18 @@ import jieba
 import numpy as np
 from scipy.linalg import norm
 
-model_file = './data/word2vec/news_12g_baidubaike_20g_novel_90g_embedding_64.bin'
-model = gensim.models.KeyedVectors.load_word2vec_format(model_file, binary=True)
+
+def prep_vector():
+    """
+    极度耗时，注意一开始准备好。
+    :return:
+    """
+    model_file = './data/news_12g_baidubaike_20g_novel_90g_embedding_64.bin'
+    model = gensim.models.KeyedVectors.load_word2vec_format(model_file, binary=True)
+    return model
 
 
-def vector_similarity(s1, s2):
+def vector_similarity(model, s1, s2):
     def sentence_vector(s):
         words = jieba.lcut(s)
         v = np.zeros(64)
@@ -17,5 +24,5 @@ def vector_similarity(s1, s2):
         return v
 
     v1, v2 = sentence_vector(s1), sentence_vector(s2)
-    print(v1, v2)
     return np.dot(v1, v2) / (norm(v1) * norm(v2))
+
